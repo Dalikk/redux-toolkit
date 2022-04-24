0c62a99c38a2ed650f118058617a8f33a80efb8c
Dalikk <dmitriev.alik1999@gmail.com>
2022-04-24 23:49:13 +0900

    сериализация

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,13 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { 
     persistStore, 
-    persistReducer, 
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
@@ -20,6 +26,12 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
