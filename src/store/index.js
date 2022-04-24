cafbd6429061b8cf2ab1d89369b42b08827f7521
Dalikk <dmitriev.alik1999@gmail.com>
2022-04-24 23:47:21 +0900

    Подготовка redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { 
+    persistStore, 
+    persistReducer, 
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import todoReducer from './todoSlice';
 
-export default configureStore({
-    reducer: {
-        todos: todoReducer,
-    },
+const rootReducer = combineReducers({
+    todos: todoReducer,
 });
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
+export default store;
